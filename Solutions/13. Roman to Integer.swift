bb15348f585d2831af9d2de8d7a58eff69c18251
Steven Lamphear <stevenlamphear@mac.com>
2021-08-15 23:24:11 -0500

    Completed problem 13. Roman to Integer

--- Solutions/13. Roman to Integer.swift
+++ Solutions/13. Roman to Integer.swift
@@ -1,18 +1,64 @@
 /*
  Problem 13: Roman to Integer
  https://leetcode.com/problems/roman-to-integer/
- 
- This solution is a work in progress. It is currently just a stub.
  */
 
 //MARK: Solution
 
+extension Character {
+    func romanNumeralValue(whenFollowedBy nextValue: Character?) -> Int {
+        if self == "I" {
+            if let next = nextValue, (next == "V" || next == "X") {
+                return -1
+            } else {
+                return 1
+            }
+        } else if self == "V" {
+            return 5
+        } else if self == "X" {
+            if let next = nextValue, (next == "L" || next == "C") {
+                return -10
+            } else {
+                return 10
+            }
+        } else if self == "L" {
+            return 50
+        } else if self == "C" {
+            if let next = nextValue, (next == "D" || next == "M") {
+                return -100
+            } else {
+                return 100
+            }
+        } else if self == "D" {
+            return 500
+        } else if self == "M" {
+            return 1000
+        }
+        
+        //No other characters are allowed given the problem's constraints.
+        return 0
+    }
+}
+
 class Solution {
     func romanToInt(_ s: String) -> Int {
+        //Trivial case: String is empty.
+        guard !s.isEmpty else { return 0 }
+        
         var total = 0
-
-        for index in Array(s.indices) {
-            print("index \(index)")
+        
+        for index in s.indices {
+            let character = s[index]
+            let nextValue: Character?
+            
+            if s.index(after: index) < s.endIndex {
+                let nextIndex = s.index(after: index)
+                nextValue = s[nextIndex]
+            } else {
+                nextValue = nil
+            }
+            
+            total += character.romanNumeralValue(whenFollowedBy: nextValue)
         }
         
         return total
