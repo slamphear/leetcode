b520530c8eed795429d917e45b7fb54a3610193b
Steven Lamphear <stevenlamphear@mac.com>
2021-08-17 23:21:24 -0500

    Completed problem 6: ZigZag Conversion

--- Solutions/6. ZigZag Conversion.swift
+++ Solutions/6. ZigZag Conversion.swift
@@ -1,14 +1,45 @@
 /*
  Problem 6: ZigZag Conversion
  https://leetcode.com/problems/zigzag-conversion/
- This solution is a work in progress. It is currently just a stub.
  */
 
 //MARK: Solution
 
 class Solution {
     func convert(_ s: String, _ numRows: Int) -> String {
-        return ""
+        //Trivial case: numRows is 1
+        guard numRows > 1 else { return s }
+        
+        //Prepare for loop
+        var rows = Array(repeating: "", count: numRows)
+        var incrementRowIndex = true
+        var rowIndex = 0
+        
+        //Build out the rows
+        for character in Array(s) {
+            rows[rowIndex] += String(character)
+            
+            //Prepare for next iteration
+            guard incrementRowIndex else {
+                if rowIndex == 0 {
+                    incrementRowIndex.toggle()
+                    rowIndex += 1
+                } else {
+                    rowIndex -= 1
+                }
+                continue
+            }
+            
+            guard rowIndex != (numRows - 1) else {
+                incrementRowIndex.toggle()
+                rowIndex -= 1
+                continue
+            }
+            
+            rowIndex += 1
+        }
+        
+        return rows.joined()
     }
 }
 
@@ -25,7 +56,8 @@ let solution = Solution()
 let testCases = [
     TestCase(s: "PAYPALISHIRING", numRows: 3, output: "PAHNAPLSIIGYIR"),
     TestCase(s: "PAYPALISHIRING", numRows: 4, output: "PINALSIGYAHRPI"),
-    TestCase(s: "A", numRows: 1, output: "A")
+    TestCase(s: "A", numRows: 1, output: "A"),
+    TestCase(s: "AB", numRows: 1, output: "AB")
 ]
 
 for testCase in testCases {
